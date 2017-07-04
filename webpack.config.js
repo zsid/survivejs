4645412349158cdfcaf10982b44463aa10d891a8
Zhivko Siderov <zhivko.siderov@gmail.com>
2017-07-04 22:02:51 +0100

    Basic config splitting for production and development

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,40 @@ const commonConfig = {
   ]
 }
 
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+  const config = {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. Good for complex setups.
+    historyApiFallback: true,
+
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+
+
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Docker, Vagrant or Cloud9, set
+    // host: options.host || '0.0.0.0';
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+  };
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config
+  )
+}
+
 module.exports = (env) => {
-  console.log('env:', env);
-  return commonConfig;
+  if (env === 'production') {
+    return productionConfig();
+  }
+
+  return developmentConfig();
 }
